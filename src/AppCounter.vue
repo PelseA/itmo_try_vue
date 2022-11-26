8960fa6df44d6a91e7b1f8b71fc9c6ebdbbafed4
Aleksandra Pelse <shashka_aya@mail.ru>
2022-11-26 13:28:41 +0300

    add AppCounter.vue, CounterValue.vue

---
+++ src/AppCounter.vue
@@ -0,0 +1,49 @@
+<template>
+  <h1>App Counter</h1>
+  <p v-html="innerHtml"></p>
+  <CounterValue
+      v-for="obj in [ {index: 1, text: 'Clicked'} ]"
+      :title="obj.text"
+      :value="counter"
+      :key="obj.index"
+  />
+  <button v-on:click="onPlus">+</button>
+  <button v-if="canRenderMinusButtonVisible" @click="onMinus">-</button>
+</template>
+
+<script>
+
+import CounterValue from "./components/CounterValue.vue";
+export default {
+  components: {CounterValue},
+
+  data() {
+    return {
+      counter: 0,
+      innerHtml: `<h2>hello</h2>`,
+    }
+  },
+  computed: {
+    canRenderMinusButtonVisible() {
+      return this.counter > 0;
+    }
+  },
+  methods: {
+    onPlus() {
+      this.counter++;
+    },
+    onMinus() {
+      this.counter--;
+    },
+  }
+};
+</script>
+
+<!-- scoped - css будет применен только на этой странице -->
+<style scoped>
+p.counter-value {
+  color: red;
+  font-size: 2rem;
+  font-weight: bold;
+}
+</style>
---
+++ src/components/CounterValue.vue
@@ -0,0 +1,23 @@
+<template>
+  <p class="value">
+    <span style="color: blue;">{{ title }}</span>: {{ value }}
+  </p>
+</template>
+
+<script>
+export default {
+  props: {
+    title: '',
+    value: 0,
+  },
+};
+</script>
+
+<!-- scoped - css будет применен только на этой странице -->
+<style scoped>
+p.value {
+  color: red;
+  font-size: 2rem;
+  font-weight: bold;
+}
+</style>
